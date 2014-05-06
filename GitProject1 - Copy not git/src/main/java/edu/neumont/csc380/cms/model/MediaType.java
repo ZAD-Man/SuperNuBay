c97019e516e7c6d2cc6bba534bd3c2b58688175a
Drew Cottrill <wobbuffet52@gmail.com>
2014-05-06 10:24:00 -0600

    Added my project

---
+++ GitProject1 - Copy not git/src/main/java/edu/neumont/csc380/cms/model/MediaType.java
@@ -0,0 +1,38 @@
+package edu.neumont.csc380.cms.model;
+
+public enum MediaType {
+	JPEG(false), PNG(false), MOV(true), MP4(true);
+
+	public boolean isVideo;
+
+	MediaType(boolean isVideo) {
+		this.isVideo = isVideo;
+	}
+
+	public boolean isVideo() {
+		return isVideo;
+	}
+
+	public String toMIME() {
+		String type = this == MOV ? "quicktime" : this.toString();
+		return (isVideo ? "video" : "image") + "/" + type;
+	}
+
+	public static MediaType parse(String mimeType) {
+        /*
+		switch (mimeType.toLowerCase()) {
+		case ("image/png"):
+			return PNG;
+		case ("image/jpeg"):
+			return JPEG;
+		case ("video/mp4"):
+			return MP4;
+		case ("video/quicktime"):
+			return MOV;
+		default:
+			throw new BadMimeTypeException();
+		}
+		*/
+        return PNG;
+	}
+}
