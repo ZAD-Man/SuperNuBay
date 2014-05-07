c60935afb6b07b8cfd1272c4a5cc2c3c854bec63
Drew Cottrill <wobbuffet52@gmail.com>
2014-05-07 10:19:49 -0600

    Added CMSService

---
+++ AuctionServer/src/main/java/edu/neumont/csc380/AuctionServer/CMSService.java
@@ -0,0 +1,51 @@
+package edu.neumont.csc380.AuctionServer;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import edu.neumont.csc380.cms.service.MediaService;
+import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
+
+/**
+ * Created by acottrill on 5/7/2014.
+ */
+public class CMSService {
+    private MediaService service = JAXRSClientFactory.create(
+            "http://localhost:8080/CMS-server/api", MediaService.class,
+            Arrays.asList(new JacksonJaxbJsonProvider()));
+
+    public Response getMedia(Long id) {
+        return service.getMedia(id);
+    }
+
+    public Response getMediaData(Long id) {
+        return service.getMediaData(id);
+    }
+
+    public Response getMediadata(Long id) {
+        return service.getMediaThumb(id);
+    }
+
+    public void deleteMedia(Long id) {
+        service.deleteMedia(id);
+    }
+
+    public Response getUserMedia(Long id) {
+        return service.getMediaByUser(id);
+    }
+
+    public Response getAuctionMedia(Long id) {
+        return service.getMediaByAuction(id);
+    }
+
+    public void setUserProfilePicture(Long id, String caption, byte[] data,
+                                      String mimeType) {
+        service.setUserProfilePicture(id, caption, data, mimeType);
+    }
+
+    public void addAuctionMedia(Long id, String caption, byte[] data,
+                                String mimeType) {
+        service.addAuctionMedia(id, caption, data, mimeType);
+    }
+}
