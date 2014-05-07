7cd064b2c5b54c7a44cfddbad236d45766737b41
Zach Durtschi <The.ZAD.One@GMail.com>
2014-05-07 07:44:03 -0600

    Use auth in profile?
    Theoretically the profile should be going through auth now, but it gives
    ClassNotFoundExceptions, so something's not quite right...

--- ProfileJavaAPI/hellorest/src/main/java/edu/neumont/csc380/profile/handler/ProfileRequestHandler.java
+++ ProfileJavaAPI/hellorest/src/main/java/edu/neumont/csc380/profile/handler/ProfileRequestHandler.java
@@ -1,5 +1,6 @@
 package edu.neumont.csc380.profile.handler;
 
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 import javax.ws.rs.core.Response;
@@ -9,37 +10,35 @@ import org.apache.cxf.jaxrs.ext.RequestHandler;
 import org.apache.cxf.jaxrs.model.ClassResourceInfo;
 import org.apache.cxf.message.Message;
 
+import edu.neumont.csc380.AuthClient.AuthClient;
+import edu.neumont.csc380.AuthClient.AuthResponse;
+
 @Provider
 public class ProfileRequestHandler implements RequestHandler {
 
 	public Response handleRequest(Message message, ClassResourceInfo cri) {
-		System.out.println("message.size: " + message.size());
 		String requestMethod = (String) message.get(message.HTTP_REQUEST_METHOD);
+		String requestURI = (String) message.get(message.REQUEST_URI);
+		AuthClient authClient = new AuthClient();
 		
+		TreeMap<String,Object> protocolHeaders =  (TreeMap<String, Object>) message.get(message.PROTOCOL_HEADERS);
 		
-		TreeMap<String,Object> thisThing =  (TreeMap<String, Object>) message.get(message.PROTOCOL_HEADERS);
-		System.out.println("thisThing: "+thisThing);
+		ArrayList al = (ArrayList) protocolHeaders.get("Authorization");
+		String authToken = (String) al.get(0);
 		
-		Object o = thisThing.get("Authorization");
-		System.out.println(o);
-		
-		if (requestMethod.equals("POST")) {
-			System.out.println("It's a POST!");
-			//Check auth
+		if (requestMethod.equals("PUT") || requestMethod.equals("DELETE")) {
+			if (authToken == null) {
+				return Response.status(401).build();
+			}
+			String userId = requestURI.split("/")[3];
+			AuthResponse actionOK = authClient.checkAuth(userId, authToken);
+			if (actionOK.isSuccess()) {
+				return null;
+			} else {
+				return Response.status(401).build();
+			}
+		} else {
+			return null;
 		}
-		else if (requestMethod.equals("PUT")) {
-			System.out.println("It's a PUT!");
-			//Check auth
-			
-		} else if (requestMethod.equals("DELETE")) {
-			System.out.println("It's a DELETE!");
-			//Check auth
-		}
-		
-		System.out.println("Auth \"accepted\"!");
-		return null;
 	}
-/***
- * Need to use 401 (no auth) and 403 (forbidden, incorrect auth)
- */
 }
