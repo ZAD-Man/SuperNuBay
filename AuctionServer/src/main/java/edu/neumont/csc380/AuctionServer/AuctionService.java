db5abbfd9df3c7175eca8f73147a6d585091b793
David Arellano <david.arellano@outlook.com>
2014-05-08 09:36:31 -0600

    Interface Update

--- AuctionServer/src/main/java/edu/neumont/csc380/AuctionServer/AuctionService.java
+++ AuctionServer/src/main/java/edu/neumont/csc380/AuctionServer/AuctionService.java
@@ -4,93 +4,41 @@ import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Service;
 
-import edu.neumont.csc380.AuthServer.AuthPayload;
-import edu.neumont.csc380.AuthServer.Credentials;
-import edu.neumont.csc380.Database.AuthorizationToken;
-import edu.neumont.csc380.Exceptions.NoIDException;
-import edu.neumont.csc380.Exceptions.NoUserFoundException;
-import edu.neumont.csc380.Exceptions.TokenExpiredException;
-import edu.neumont.csc380.Exceptions.TokenInvalidException;
-import edu.neumont.csc380.Exceptions.TokenMissingException;
+import edu.neumont.csc380.Database.Bid;
+import edu.neumont.csc380.Database.Item;
 
 @Service("IAuctionService")
 public class AuctionService implements IAuctionService {
+
+	@Override
+	public Response getItem(Long id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response createItem(Item item) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response updateItem(Long id, Item item) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response deleteItem(Long id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response placeBid(Bid bid) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
-	public Response handleLoginRequest(Credentials credentials) throws Exception {		
-		Response response = null;
-		return response;
-	}
-
-	public Response handleLogoutRequest(AuthPayload payload) throws Exception {
-		return Response.status(204).build();
-	}
-
-	//TODO: This needs to create a user not check if one exists
-	public Response handleCreationRequest(Credentials creds) throws Exception {
-		Response response = null;
-		return response;
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.neumont.csc380.AuthServer.IAuthenticationService#verify(edu.neumont.csc380.AuthServer.AuthPayload)
-	 */
-	public Response verify(AuthPayload auth) throws Exception {
-		Response response = null;
-		return response;
-	}
-
-	/**
-	 * Generates a response after validating the token
-	 * The response will have a AuthPayload with only the id
-	 * 
-	 * @param userID
-	 * @return response with an AuthPayload
-	 */
-	private Response generateResponse(String userID) {
-		Response response = null;
-		return response;
-	}
-
-	/**
-	 * Generates and updates a token in the database
-	 * @param userID
-	 * @return
-	 */
-	private String updateToken(String userID) {
-		String tokenString = null;
-		return tokenString;
-	}
-
-	/**
-	 * Validates the token provided is valid
-	 * Checks the userID, Signature, and Expiry of a token match
-	 * @param userID
-	 * @param userToken
-	 * @throws TokenInvalidException
-	 * @throws TokenExpiredException
-	 * @throws TokenMissingException
-	 */
-	private AuthorizationToken validateUserToken(String userID, AuthorizationToken userToken) throws Exception {
-		AuthorizationToken dbToken =  null;
-		return dbToken;
-	}
-
-	/**
-	 * Will verify the user exists in the database
-	 * @param auth
-	 * @throws NoUserFoundException
-	 */
-	private void verifyUserExists(AuthPayload auth) throws NoUserFoundException {
-
-	}
-
-	/**
-	 * Checks the user is not null or empty
-	 * 
-	 * @param id
-	 * @throws NoIDException
-	 */
-	private void verifyIdExists(String id) throws NoIDException {
-		
-	}
+	
 }
--- AuctionServer/src/main/java/edu/neumont/csc380/AuctionServer/IAuctionService.java
+++ AuctionServer/src/main/java/edu/neumont/csc380/AuctionServer/IAuctionService.java
@@ -1,40 +1,47 @@
 package edu.neumont.csc380.AuctionServer;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import org.apache.cxf.jaxrs.model.wadl.ElementClass;
-
-import edu.neumont.csc380.AuthServer.AuthPayload;
-import edu.neumont.csc380.AuthServer.Credentials;
+import edu.neumont.csc380.Database.Bid;
+import edu.neumont.csc380.Database.Item;
 
 @Path("/auctionServer")
 public interface IAuctionService {
 	
-	@POST
+	@GET
 	@Consumes("application/vnd.neumont.auction.edu-v1+json")
-	@Path("/login")
+	@Path("/item/{itemId}")
 	@Produces("application/json")
-	@ElementClass(request = Credentials.class)
-	Response handleLoginRequest(Credentials credentials)  throws Exception;
-
-	@POST
-	@Consumes("application/vnd.neumont.auction.edu-v1+json")
-	@Path("/logout")
-	Response handleLogoutRequest(AuthPayload payload)  throws Exception;
+	Response getItem(@PathParam("itemId") Long id) throws Exception;
 	
 	@POST
 	@Consumes("application/vnd.neumont.auction.edu-v1+json")
-	@Path("/create")
+	@Path("/item")
 	@Produces("application/json")
-	Response handleCreationRequest(Credentials creds)  throws Exception;
+	Response createItem(Item item) throws Exception;
+	
+	@PUT
+	@Consumes("application/vnd.neumont.auction.edu-v1+json")
+	@Path("/item/{itemId}")
+	@Produces("application/json")
+	Response updateItem(@PathParam("itemId") Long id, Item item)  throws Exception;
+	
+	@DELETE
+	@Consumes("application/vnd.neumont.auction.edu-v1+json")
+	@Path("/item/{itemId}")
+	Response deleteItem(@PathParam("itemId") Long id) throws Exception;
 	
 	@POST
 	@Consumes("application/vnd.neumont.auction.edu-v1+json")
-	@Path("/verify")
+	@Path("/item/{itemId}/bid")
 	@Produces("application/json")
-	Response verify(AuthPayload auth)  throws Exception;
+	Response placeBid(Bid bid) throws Exception;
 }
 
